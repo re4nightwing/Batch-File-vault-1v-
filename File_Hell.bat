36a70ab085d019f2e60d60875164258ad7e0cdd7
Dulan Pabasara <52290785+re4nightwing@users.noreply.github.com>
2019-10-17 14:23:36 +0530

    Add files via upload

---
+++ File_Hell.bat
@@ -0,0 +1,45 @@
+@ECHO OFF
+    title File Hell 1.2v
+    color 0a
+if EXIST "File Hell" goto WalkIn
+if NOT EXIST "File Hell" goto MakeDir
+:CONFIRM
+    echo .
+echo Are you sure want to Obsolete the Files ?(Y/N)
+set/p "cho=>"
+if %cho%==Y goto HELL
+if %cho%==y goto HELL
+if %cho%==N goto RETURN
+if %cho%==n goto RETURN
+    echo .
+echo Invalid Choice.Please enter Y/N as you wish
+goto CONFIRM
+:HELL
+attrib +h +s
+echo Your Files are in hell but not folders
+goto End
+:RETURN
+attrib -h -s
+echo Your Files has been Restored
+goto End
+:WalkIn
+cd File Hell
+goto CONFIRM
+:MakeDir
+    echo -
+echo You have no folder called 'File Hell' would you like to create one here?(Y/N)
+set/p "ans=>"
+if %ans%==y goto CREATE
+if %ans%==Y goto CREATE
+if %ans%==n goto End
+if %ans%==N goto End
+    echo -
+echo Invalid Choice.Please enter Y/N as you wish.
+goto MakeDir
+:CREATE
+md "File Hell"
+    echo -
+echo File Hell Folder has been created Successfully.
+    echo -
+goto WalkIn
+:End
